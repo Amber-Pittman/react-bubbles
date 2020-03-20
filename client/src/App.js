4b1d149f8ed1dabcb5c5b5f12370e8a19b063f5c
Amber-Pittman <amber@amberpittman.com>
2020-03-20 19:40:18 -0400

    Working on Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// #3 Define some routes by importing from rrd
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// #7 import the Login component that was created
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+
+
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+
+        {/*  #4 Let's create a nav tag and create some links */}
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+
+        {/* #5 Define some routes */}
+        {/* #8 Add Login component to login Route */}
+        <Route exact path="/login" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,30 @@
 import React from "react";
 
+// #6 Create Signin Component
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
     
   return (
-      <>
+    // #9 Return a form tag
+      <form>
         <h1>Welcome to the Bubble App!</h1>
-        <p>Build a login page here</p>
-      </>
+
+        {/* #10 Create some input fields and a button for the form */}
+        <input
+          type="email"
+          autoComplete="email"
+          name="email"
+          placeholder="Email" />
+        
+        <input
+          type="password"
+          autoComplete="current-password"
+          name="password"
+          placeholder="Password" />
+
+        <button type="submit">Login</button>
+      </form>
     );
 };
 
